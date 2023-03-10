4433d25fc348cdceeee137632839d49b45f77c5b
Brais <braisfv22@gmail.com>
2023-03-10 18:12:38 +0100

    Se cambia la manera de guardar los archivos y se hace metodo para train

--- main.py
+++ main.py
@@ -3,24 +3,48 @@ import mediapipe as mp
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+num = [i for i in range(0, 21)]
+columns = [f"{j}{i}" for i in num for j in ["x", "y"]]
+columns = ["frame", "position"] + columns
+
+
+def guardar_dataframe_vacio():
+    # check if file exists
+    output_str = f"datasets/{datetime.now().strftime('%Y-%m-%d')}"
+    csv_output_dir = Path(output_str)
+    csv_output_dir.mkdir(parents=True, exist_ok=True)
+    csv_filename = f"{datetime.now().strftime('%Y-%m-%d')}.csv"
+    if (csv_output_dir / csv_filename).exists():
+        return
+    df = pd.DataFrame(columns=columns)
+    df.to_csv(csv_output_dir / csv_filename, mode='a', header=True, index=False)
+
+
+def crear_linea_df(df, hand_landmarks):
+    for hand_landmark in hand_landmarks:
+        for point_id, landmark in enumerate(hand_landmark.landmark):
+            # where the frame is frame_num set x and y
+            df.loc[f"x{point_id}"] = landmark.x
+            df.loc[f"y{point_id}"] = landmark.y
+    return df
 
 
 def guardar_datos_frame(hand_landmarks, frame, position_name, frame_num):
-    df = pd.DataFrame(columns=["id", "x", "y"])
-    for hand_landmark in hand_landmarks:
-        for point_id, landmark in enumerate(hand_landmark.landmark):
-            # save to row of dataframe
-            df = df.append({
-                "id": point_id,
-                "x": landmark.x,
-                "y": landmark.y
-            }, ignore_index=True)
-            pass
-    output_str = f"datasets/{datetime.now().strftime('%Y-%m-%d')}/{position_name}"
+    guardar_dataframe_vacio()
+    df = pd.DataFrame(columns=columns)
+    df = df.append({
+        "frame": frame_num,
+        "position": position_name,
+    }, ignore_index=True)
+    df = crear_linea_df(df, hand_landmarks)
+    output_str = f"datasets/{datetime.now().strftime('%Y-%m-%d')}"
     # save to csv
     csv_output_dir = Path(output_str)
     csv_output_dir.mkdir(parents=True, exist_ok=True)
-    csv_filename = f"{position_name}.csv"
+    csv_filename = f"{datetime.now().strftime('%Y-%m-%d')}.csv"
     df.to_csv(csv_output_dir / csv_filename, mode='a', header=False, index=False)
     # save to image
     frames_output_dir = output_str + "/frames/"
@@ -30,7 +54,7 @@ def guardar_datos_frame(hand_landmarks, frame, position_name, frame_num):
     Img.guardar_imagen(frame, (frames_output_dir + frame_name))
 
 
-def mostrar_puntos_mano(frame, params):
+def mostrar_puntos_mano_entrenar(frame, params):
     frame_num = params["frame_num"]
 
     hands = params["hands"]
@@ -54,10 +78,46 @@ def mostrar_puntos_mano(frame, params):
             hand_landmarks,
             frame,
             position_name,
-            frame_num
+            frame_num,
         )
 
     return frame
+
+
+def mostrar_puntos_mano_jugar(frame, params):
+    frame_num = params["frame_num"]
+
+    hands = params["hands"]
+    results = hands.process(image=frame)
+    hand_landmarks = results.multi_hand_landmarks
+    Img.escribir(frame, pos=(20, 20), size=4, text=f"Frame: {frame_num}")
+    if not hand_landmarks:
+        return frame
+    modelo = params["modelo"]
+    df = pd.DataFrame(columns=columns)
+    df.loc["frame"] = 1
+    df = crear_linea_df(df, hand_landmarks)
+    df = df.drop(columns=["position"])
+    df = df.drop(columns=["frame"])
+    prediction = modelo.predict(df)
+    Img.escribir(frame, pos=(20, 50), size=4, text=f"Prediction: {prediction[0]}")
+
+    return frame
+
+
+def entrenar_clasificador():
+    df = pd.read_csv(f"datasets/{datetime.now().strftime('%Y-%m-%d')}/{datetime.now().strftime('%Y-%m-%d')}.csv")
+    df = df.drop(columns=["frame"])
+    y = df["position"]
+    x = df.drop(columns=["position"])
+
+    # Clasificador
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(x_train, y_train)
+
+    print(clf.score(x_test, y_test))
+    return clf
 
 
 def obtener_datos():
@@ -70,19 +130,39 @@ def obtener_datos():
             min_tracking_confidence=0.6
     ) as hands:
         Camera.video_capture(
-            operacion=mostrar_puntos_mano,
+            operacion=mostrar_puntos_mano_entrenar,
             operacion_params={
                 "hands": hands,
                 "mp_hand": mp_hand,
                 "mp_drawing": mp_drawing,
-                "position_name": position
+                "position_name": position,
+            }
+        )
+
+
+def jugar(modelo):
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hand = mp.solutions.hands
+    with mp_hand.Hands(
+            max_num_hands=1,
+            min_detection_confidence=0.3,
+            min_tracking_confidence=0.6
+    ) as hands:
+        Camera.video_capture(
+            operacion=mostrar_puntos_mano_jugar,
+            operacion_params={
+                "hands": hands,
+                "mp_hand": mp_hand,
+                "mp_drawing": mp_drawing,
+                "modelo": modelo,
             }
         )
 
 
 def menu():
     print("1. Obtener datos de entrenamiento")
-    print("2. Jugar")
+    print("2. Entrenar clasificador")
+    print("3. Jugar")
     print("0. Salir")
     try:
         return int(input("Elija una opcion: "))
@@ -91,6 +171,7 @@ def menu():
 
 
 def main():
+    modelo = None
     while True:
         opcion = menu()
         if opcion == 0:
@@ -98,7 +179,13 @@ def main():
         elif opcion == 1:
             obtener_datos()
         elif opcion == 2:
+            modelo = entrenar_clasificador()
             pass
+        elif opcion == 3:
+            if modelo is None:
+                print("Primero debes entrenar el clasificador")
+                continue
+            jugar(modelo)
         else:
             print("Opcion invalida")
 
